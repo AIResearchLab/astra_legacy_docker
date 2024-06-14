8eaf2a6f72a6659c6910bb938c8f087540afbe93
Kalana Ratnayake <kalanaratnayake95@gmail.com>
2024-06-15 01:43:02 +1000

    Update workspace_entrypoint.sh

--- workspace_entrypoint.sh
+++ workspace_entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-wget https://raw.githubusercontent.com/AIResearchLab/astra_legacy_ros/main/astra_camera/scripts/56-orbbec-usb.rules
-cp 56-orbbec-usb.rules /etc/udev/rules.d/56-orbbec-usb.rules
 udevadm control --reload-rules
 udevadm trigger
 
